ef60f0af7476bcfb46992309fc6614ae67bbcdd7
Lucas Santana <lucassantan97@gmail.com>
2025-12-09 16:37:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2573154388-af57e",
+  "appId": "1:668721844660:web:e7340b697a1ec839ee22e1",
+  "apiKey": "AIzaSyCw5jukXxEWXi8Oe6bm0LKisBWqUwCSzrk",
+  "authDomain": "studio-2573154388-af57e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "668721844660"
+};
