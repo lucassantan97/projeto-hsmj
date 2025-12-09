35a1a88b6c3dfe5f73085270ab38b992a2395bd8
Lucas Santana <lucassantan97@gmail.com>
2025-12-09 18:12:18 +0000

    atualize o logotipo da mj locadora por este

--- src/components/icons/mj-logo.tsx
+++ src/components/icons/mj-logo.tsx
@@ -3,11 +3,11 @@ import { cn } from "@/lib/utils";
 export function MjLogo({ className }: { className?: string }) {
   return (
     <div className={cn("relative w-[60px] h-[50px] flex items-center justify-center", className)}>
-      <img 
-        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAoCAYAAACrUDmFAAACXklEQVR4nO2YvWtUQRjGnz2p3YQoihgsbGz9CjY+QD+AZMF/YGFhYWWWloVdLAQLG8V/YGNhYSAIgpVFLRQD0bCwUYyJk4hf/BGh8w7c7e5u3r37DtwzMw/ffmfe2ZnF6bS6sClnFTOZHAYCgX5isbhc3kCgRNCr2h+8Z+f8bY/X69UaNzc3XcbjcfVwDhzAm3N9Go12qNfrZ/htgP5/DOB5qSSTyXo+n99c2scyU/gS4O3t7fVlKBTyK/zlsYJgB17Lso3W2V5W2TCA6XRa9ff3m6vA9HAhOD4+Pnl5eXn4VwG+DfgLcLgJgT2B8x2EAJSgHc34zC94wVf+s/01gAOCg0I1v8b+4eERgQJsm+L7vWxtbZkBHIZwTg+A1Wp1oHw+v3NxcfGL6el/AqxWC4jFYv8r/BfS9/f3h9PpvKswGvH4sOMKAPwV8JmBgNvt9gUAAIB2u50vAOcBvj+eZUlW9fX1pciy7E8L/gXwO4DP0xAg9q/xG4BOJpMtwzAUAIaGhq6j0WgrDBBCdDqdRyiKCnwB8D+gC4jGje+NfHh4+PH5+fkJgAt4HMBjBODLsixLz/O1gJ+J+N4H2yEAmUzWT09PDwI4D+B/i5fUArBH4uPjY8+y7BfH4/FLAacAPgDwXk+gVADs+xbgC4AnAZ4GuBHgNvz10Gg0dmpqanq/TqenpycAzwD8D+B+gMWKA2CvA/gMwF+APutHQ5/DMNQRhuHd7pS/APpA+6/pBwZElAAAAABJRU5ErkJggg==" 
-        alt="MJ Locadora Logo" 
-        className="w-full h-full object-contain"
-      />
+      <svg width="60" height="50" viewBox="0 0 78 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M54.593 56.1102C66.8624 56.1102 76.702 46.417 76.702 34.3313C76.702 22.2455 66.8624 12.5524 54.593 12.5524C42.3236 12.5524 32.484 22.2455 32.484 34.3313C32.484 46.417 42.3236 56.1102 54.593 56.1102Z" fill="#EA702E"/>
+        <path d="M22.8468 43.1539C35.1162 43.1539 44.9558 33.4608 44.9558 21.375C44.9558 9.28923 35.1162 -0.40387 22.8468 -0.40387C10.5774 -0.40387 0.737793 9.28923 0.737793 21.375C0.737793 33.4608 10.5774 43.1539 22.8468 43.1539Z" fill="#A1A1A1"/>
+        <path d="M-5.32178 30.2949H12.9231L16.9529 26.307H36.3197M79.7318 30.2949H61.487L57.4572 26.307H38.0904" stroke="white" strokeWidth="3.96154" strokeDasharray="7.92 7.92"/>
+      </svg>
     </div>
   );
 }
