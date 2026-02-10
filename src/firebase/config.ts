07a1df923ef90f56c92756189c966e7d6f18171d
Lucas Santana <lucassantan97@gmail.com>
2026-02-10 18:29:43 +0000

    lançando quero que muda o nome do site para ter acesso a este sistema, p

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDdgvJF6437csT8Q1pBX2m0aYwMO8gJE8E",
-  authDomain: "sistema-hs-mj.firebaseapp.com",
-  projectId: "sistema-hs-mj",
-  storageBucket: "sistema-hs-mj.appspot.com",
+  authDomain: "hsmjfrota.firebaseapp.com",
+  projectId: "hsmjfrota",
+  storageBucket: "hsmjfrota.appspot.com",
   messagingSenderId: "1083112326908",
   appId: "1:1083112326908:web:d8c302b5e8ed7bfff1f979",
   measurementId: "G-554CWMQ8LQ"
 };
+
